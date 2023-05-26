4e55b3429b2fa419381330375411f43a3fffbdde
Denis <denis_poljak@hotmail.com>
2023-05-26 10:58:04 +0200

    Implemented automatic connection to serial port where goggle gives response

--- fpv-tests/FPV/ViewModels/FpvOneProTestViewModel.cs
+++ fpv-tests/FPV/ViewModels/FpvOneProTestViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using FPV.Core;
 using FPV.Service.Common;
+using Microsoft.Win32;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -20,13 +23,12 @@ namespace FPV.ViewModels
 
         #region Properties
 
-       
+
         #endregion
 
         #region SERIAL PORT FOR ABSTRACTION
 
         private SerialPort _serialPort;
-
 
         private string _serialPortReceivedMessage;
         public string SerialPortReceivedMessage
@@ -95,9 +97,6 @@ namespace FPV.ViewModels
             public string roll { get; set; }
         }
 
-
-
-
         private int FindClosingBraceIndex(string input, int openingBraceIndex)
         {
             int closingBraceIndex = openingBraceIndex;
@@ -131,7 +130,6 @@ namespace FPV.ViewModels
 
             return -1;
         }
-
 
         public List<JSONData> FindObjectsWithProperties(string input)
         {
@@ -167,7 +165,6 @@ namespace FPV.ViewModels
 
             return foundObjects;
         }
-
        
         private void MyParser(string stringToParse)
         {
@@ -193,6 +190,131 @@ namespace FPV.ViewModels
             }
         }
 
+
+
+        #region Automatic connection
+      
+        private async Task ConnectToSerialPortAsync()
+        {
+            try
+            {
+
+                SerialPortReceivedMessage = "";
+
+                if (_serialPort?.IsOpen == true)
+                {
+                    _serialPort.Close();
+                    SerialPortConnectionStatusMessage = "Connect";
+                    SerialPortReceivedMessage = "Serial port disconnected.";
+                }
+                else
+                {
+                    // PRO COM13
+                    // MK2 COM11
+                    var portName = await FindSerialPortWithResponseAsync();
+                    if (string.IsNullOrEmpty(portName))
+                    {
+                        SerialPortConnectionStatusMessage = "Connect";
+                        throw new Exception("Failed to find a serial port that provides a response.");
+                    }
+
+                    _serialPort = new SerialPort(portName, 115200);
+                    _serialPort.Open();
+                    if (_serialPort.IsOpen)
+                    {
+                        _serialPort.DataReceived += SerialPort_DataReceived;
+                        SerialPortConnectionStatusMessage = "Connected";
+                        _serialPort.WriteLine($"Successfully connected to serial port: {portName}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SerialPortReceivedMessage = ex.Message;
+            }
+        }
+
+
+        protected async Task<string> FindSerialPortWithResponseAsync()
+        {
+            try
+            {
+                // Data ports
+                ObservableCollection<string> detectPorts = new ObservableCollection<string>();
+
+                // Get all COM ports
+                using (ManagementClass i_Entity = new ManagementClass("Win32_PnPEntity"))
+                {
+                    foreach (ManagementObject i_Inst in i_Entity.GetInstances())
+                    {
+                        Object o_Guid = i_Inst.GetPropertyValue("ClassGuid");
+                        if (o_Guid == null || o_Guid.ToString().ToUpper() != "{4D36E978-E325-11CE-BFC1-08002BE10318}")
+                            continue; // Skip all devices except device class "PORTS"
+
+                        String s_Caption = i_Inst.GetPropertyValue("Caption").ToString();
+                        String s_Manufact = i_Inst.GetPropertyValue("Manufacturer").ToString();
+                        String s_DeviceID = i_Inst.GetPropertyValue("PnpDeviceID").ToString();
+                        String s_RegPath = "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Enum\\" + s_DeviceID + "\\Device Parameters";
+                        String s_PortName = Registry.GetValue(s_RegPath, "PortName", "").ToString();
+
+                        int s32_Pos = s_Caption.IndexOf(" (COM");
+                        if (s32_Pos > 0) // remove COM port from description
+                            s_Caption = s_Caption.Substring(0, s32_Pos);
+
+                        if (s_Caption.Contains("USB-SERIAL") || s_Caption.Contains("UART") || s_Caption.Contains("USB"))
+                        {
+                            detectPorts.Add(s_PortName);
+                        }
+                    }
+                }
+
+                foreach (var port in detectPorts)
+                {
+                    _serialPort = new SerialPort(port, 115200);
+                    _serialPort.Open();
+
+                    if (_serialPort?.IsOpen == true)
+                    {
+                        _serialPort.Write("test");
+
+                        // Wait for response
+                        await Task.Delay(500);
+
+                        if (_serialPort.ReadExisting().Length > 0)
+                        {
+                            return port;
+                        }
+                    }
+                }
+                return string.Empty;
+            }
+            catch (Exception ex)
+            {
+                //This exception we handle in antoher function
+                throw ex;
+            }
+            finally
+            {
+                if (_serialPort?.IsOpen == true)
+                {
+                    _serialPort.Close();
+                }
+            }
+        }
+
+
+
+        #endregion
+
+
+
+
+
+        private void OnCommandActionExceptionHandler(Exception ex)
+        {
+            //IsErrorOccurred = true;
+            //ContentState = ExceptionHandlerViewModel.CreateView(ex.Message);
+        }
         #endregion
 
 
@@ -203,32 +325,16 @@ namespace FPV.ViewModels
 
             NavigateHomeCommand = new RelayCommand(o =>
             {
+                // Handler for serial port
+                if (_serialPort?.IsOpen == true)
+                    _serialPort.Close();
+
+                // Handler for navigate to home view
                 NavigationService.NavigateTo<HomeViewModel>();
             });
 
-            SerialPortTryToConnectCommand = new RelayCommand(o =>
-            {
-                if (_serialPort?.IsOpen == true)
-                {
-                    _serialPort.Close();
-                    SerialPortConnectionStatusMessage = "Connect";
-                    SerialPortReceivedMessage = "";
-                }
-                else
-                {
-                    // PRO COM13
-                    // MK2 COM11
-                    _serialPort = new SerialPort("COM13", 115200);
-                    _serialPort.Open();
-                    if (_serialPort.IsOpen)
-                    {
-                        _serialPort.DataReceived += SerialPort_DataReceived;
-                        SerialPortConnectionStatusMessage = "Connected";
-                    }
-                }
-            });
-           
 
+            SerialPortTryToConnectCommand = new AsyncRelayCommand(ConnectToSerialPortAsync, (ex) => OnCommandActionExceptionHandler(ex));
 
             SerialPortSendMessageCommand = new RelayCommand(o =>
             {
